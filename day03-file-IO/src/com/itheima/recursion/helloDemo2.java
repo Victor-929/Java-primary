8dea9d48d021b7d863f1bc358333662f56bc203a
Milk Wee <1140484715@qq.com>
2025-04-02 20:47:22 +0800

    update

---
+++ day03-file-IO/src/com/itheima/recursion/helloDemo2.java
@@ -0,0 +1,8 @@
+package com.itheima.recursion;
+
+public class helloDemo2 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
