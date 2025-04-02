66c509ac29cb56f1ea46ad06577351c62674d19b
weixs <1729529437@qq.com>
2025-04-02 16:38:34 +0800

    更新hellodemo1文件

--- day03-file-IO/src/com/itheima/recursion/helloDemo1.java
+++ day03-file-IO/src/com/itheima/recursion/helloDemo1.java
@@ -3,5 +3,6 @@ package com.itheima.recursion;
 public class helloDemo1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
